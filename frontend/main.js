12b2638df7ce7da03194a5ef2893a546f98cf62f
Babajide Kale <bkale09@gmail.com>
2016-07-18 13:55:15 -0400

    update

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
   var userinput = document.querySelector('.userInput');
   var location = document.querySelector('.location')
+  var gmaps = document.getElementById('map')
 
   location.addEventListener('click',function(){
     navigator.geolocation.getCurrentPosition(success, error, options);
@@ -25,10 +26,45 @@ document.addEventListener('DOMContentLoaded',function(){
     console.warn('ERROR(' + err.code + '): ' + err.message);
   };
 
-
+  var map;
+        function initMap() {
+          map = new google.maps.Map(gmaps, {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+        }
 
 
 
 
 
 });//DOMContentLoaded closing tag
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
