50da43ba770f028b2c4c2bff1611810ea64138cb
Babajide Kale <bkale09@gmail.com>
2016-07-18 15:09:56 -0400

    update

--- frontend/main.js
+++ frontend/main.js
@@ -1,37 +1,84 @@
 document.addEventListener('DOMContentLoaded',function(){
-var location = document.querySelector('.location')
-var output = document.querySelector('.output')
+var mylocation = document.querySelector('.mylocation')
+var location ={}
+var long;
+var lat;
 
-location.addEventListener('click',function(){
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
+mylocation.addEventListener('click',function(){
+
+  // This example requires the Places library. Include the libraries=places
+  // parameter when you first load the API. For example:
+  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+  var map;
+  var infowindow;
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+
+  function success(pos) {
+    var crd = pos.coords;
+    long = crd.latitude;
+    lat = crd.longitude;
+    console.log('More or less ' + crd.accuracy + ' meters.');
+    console.log(long,lat);
+
+  };
+  console.log(long,lat);
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
+
+
+
+  function initMap() {
+    var pyrmont = location;
+
+    map = new google.maps.Map(document.querySelector('#map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['store']
+    }, callback);
   }
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
 
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
 
 });//location closing tag
-
-
-
 
 
 });//DOMContentLoaded closing tag
